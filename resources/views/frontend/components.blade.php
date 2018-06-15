a0b53ed844df992f3b5cb23c6bbf2507340975f4
Senior Expert of Web & Mobile development <funnyventure@outlook.com>
2018-06-15 06:10:14 -0700

    ticke 34

--- resources/views/frontend/components.blade.php
+++ resources/views/frontend/components.blade.php
@@ -89,14 +89,14 @@
                     Animated Progress Bars
                 </div>
 
-                <div class="panel-body">
+                <!-- <div class="panel-body">
                     <div class="progress progress-striped active">
                         <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="40"
                              aria-valuemin="0" aria-valuemax="100" style="width: 40%">
                             <span class="sr-only">40% Complete (success)</span>
                         </div>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
         <div class="col-md-6">
